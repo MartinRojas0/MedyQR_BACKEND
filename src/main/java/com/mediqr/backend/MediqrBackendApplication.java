7dc4c64bdd7ddb3f4c181404ab5900d336ef4901
Martin <165443421+MartinRojas0@users.noreply.github.com>
2026-08-30 11:46:11 -0500

    initial backed configuration - configuracion inicial del backend

---
+++ src/main/java/com/mediqr/backend/MediqrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mediqr.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediqrBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediqrBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mediqr/backend/MediqrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediqr.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediqrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
